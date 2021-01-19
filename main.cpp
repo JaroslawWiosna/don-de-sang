d11482e6817b726339d813590ccc334c4ac0273b
Jaroslaw Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-01-19 10:39:24 +0100

    Make donation printable

--- main.cpp
+++ main.cpp
@@ -4,13 +4,32 @@
 
 using namespace aids;
 
-struct Db {
+struct Donation {
    struct tm t{};
    String_View desc{};
 };
 
-void parse_db_file(String_View db_file) {
-    Db db{};
+void print1(FILE *stream, Donation don) {
+    print(stream, '{');
+
+    char buf[255];
+    strftime(buf, sizeof(buf), "%d %b %Y", &don.t);
+    print(stream, buf);
+    print(stream, ", ");
+    print(stream, don.desc);
+    print(stream, '}');
+    print(stream, '\n');
+}
+
+template <typename T>
+void print1(FILE *stream, Dynamic_Array<T> d) {
+    for (size_t i{}; i < d.size; ++i) {
+        print(stream, i+1, ". ", d.data[i]);
+    } 
+}
+
+Dynamic_Array<Donation> parse_db_file(String_View db_file) {
+    Dynamic_Array<Donation> donations{};
     while (db_file.count > 0) {
         auto line = db_file.chop_by_delim('\n');
         line = line.trim();
@@ -21,12 +40,21 @@ void parse_db_file(String_View db_file) {
         line = line.trim();
         auto desc = line.chop_word();
 
-        println(stdout, "\t|\t date: ", date, " ", "desc: ", desc, "\t|");
+        struct tm tm;
+
+        memset(&tm, 0, sizeof(struct tm)); 
+        char buf0[255];
+        memcpy(buf0, date.data, date.count);
+        strptime(buf0, "%Y-%m-%d", &tm);
+
+        donations.push({tm, desc});
     }
+    return donations;
 }
 
 int main() {
     auto db_file = read_file_as_string_view("db.txt").value_or({});
 
-    parse_db_file(db_file);
+    auto donations = parse_db_file(db_file);
+    println(stdout, donations);
 }

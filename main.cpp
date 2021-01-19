570e9716c94473b06053fd41c5823cc23c414c78
Jaroslaw Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-01-19 10:04:14 +0100

    Parse date and desc

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <time.h>
 #include "aids.hpp"
 
 using namespace aids;
 
-void parse_db(String_View db) {
-    while (db.count > 0) {
-        auto line = db.chop_by_delim('\n');
+struct Db {
+   struct tm t{};
+   String_View desc{};
+};
+
+void parse_db_file(String_View db_file) {
+    Db db{};
+    while (db_file.count > 0) {
+        auto line = db_file.chop_by_delim('\n');
         line = line.trim();
         if (line.count == 0) {
             continue;
         }
-        println(stdout, "\t|\t", line, "\t|\t");
+        auto date = line.chop_word();
+        line = line.trim();
+        auto desc = line.chop_word();
+
+        println(stdout, "\t|\t date: ", date, " ", "desc: ", desc, "\t|");
     }
 }
 
 int main() {
-    auto db = read_file_as_string_view("db.txt").value_or({});
+    auto db_file = read_file_as_string_view("db.txt").value_or({});
 
-    parse_db(db);
+    parse_db_file(db_file);
 }

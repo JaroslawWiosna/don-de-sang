13495fb8e39332c093f138e058175ee177a1bde1
Jaroslaw Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-01-19 09:57:01 +0100

    Read `db.txt` line-by-line

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include "aids.hpp"
+
+using namespace aids;
+
+void parse_db(String_View db) {
+    while (db.count > 0) {
+        auto line = db.chop_by_delim('\n');
+        line = line.trim();
+        if (line.count == 0) {
+            continue;
+        }
+        println(stdout, "\t|\t", line, "\t|\t");
+    }
+}
 
 int main() {
-    printf("%s:%d\n", __FILE__, __LINE__); 
+    auto db = read_file_as_string_view("db.txt").value_or({});
+
+    parse_db(db);
 }
